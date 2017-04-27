2d46959f3181b03282fb5af4825d3b2c73b50280
Focinfi <focinfi@gmail.com>
2017-04-27 21:47:55 +0800

    Update Dockerfile

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"log"
-	"os"
-
-	"github.com/Focinfi/sqs/master"
-)
+import "github.com/Focinfi/sqs/master"
 
 func main() {
-	addr := os.Getenv("SQS_MASTER_ADDR")
-	log.Println("SQS_MASTER_ADDR:", addr)
-	if addr == "" {
-		panic("Need SQS_MASTER_ADDR environment parameter")
-	}
-
-	master.NewService(addr).Start()
+	master.NewService(":8080").Start()
 }
